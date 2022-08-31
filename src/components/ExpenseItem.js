3fd45a2b3a89e10eb182a3a6ec67255f0a30beee
jakegodsall <jake.edward.godsall@gmail.com>
2022-08-31 10:41:48 +0100

    added ExpenseItem component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+import './ExpenseItem.css';
+import Calendar from './Calendar';
+
+function ExpenseItem(props) {
+
+    return (
+
+        <div className="expense-item">
+            <Calendar date={props.date} />
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">£{props.amount}</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
